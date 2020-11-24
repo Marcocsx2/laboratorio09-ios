83e95478d7c1c0479bd21070635d9b7ce6cd0ffd
Marco Vizcarra <marco.vizcarra@tecsup.edu.pe>
2020-11-23 17:58:52 -0800

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Marco Vizcarra on 11/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
